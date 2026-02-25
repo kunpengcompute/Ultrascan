4152e9f98f66cc399a3fa523c082bf328869a0b0
jiazhongfeng1 <jiazhongfeng1@huawei.com>
2026-02-25 17:17:31 +0800

    补充lilyForTeddy上报队列支持

--- src/scratch.c
+++ src/scratch.c
@@ -99,7 +99,8 @@ hs_error_t alloc_scratch(const hs_scratch_t *proto, hs_scratch_t **scratch) {
     size_t delay_region_size =
         fatbit_array_size(DELAY_SLOT_COUNT, proto->delay_fatbit_size);
 
-    size_t lily_items_store_size = proto->lily_items_capacity * sizeof(LilyMatchItem);
+    size_t lily_items_store_size = proto->lily_ctx.capacity * sizeof(LilyMatchItem);
+    size_t lily_for_teddy_items_store_size = proto->lily_for_teddy_ctx.capacity * sizeof(LilyMatchItem);
 
     // the size is all the allocated stuff, not including the struct itself
     size_t size = queue_size + 63
@@ -115,8 +116,9 @@ hs_error_t alloc_scratch(const hs_scratch_t *proto, hs_scratch_t **scratch) {
                   + som_store_size
                   + som_now_size
                   + som_attempted_size
-                  + som_attempted_store_size
-                  + lily_items_store_size + 15;
+                  + som_attempted_store_size + 15 /* 原始冗余 */
+                  + lily_items_store_size + 15
+                  + lily_for_teddy_items_store_size + 15; /* 每次16字节对齐需要预留15字节 */
 
     /* the struct plus the allocated stuff plus padding for cacheline
      * alignment */
@@ -230,14 +232,23 @@ hs_error_t alloc_scratch(const hs_scratch_t *proto, hs_scratch_t **scratch) {
     s->fullStateSize = fullStateSize;
     current += fullStateSize;
 
-    current = ROUNDUP_PTR(current, 8);
-    s->lily_items = (struct LilyMatchItem *)current;
-    if (s->lily_items_capacity == 0) {
-        s->lily_items = NULL;
+    current = ROUNDUP_PTR(current, 16);
+    s->lily_ctx.items = (struct LilyMatchItem *)current;
+    if (s->lily_ctx.capacity == 0) {
+        s->lily_ctx.items = NULL;
     }
-    s->lily_items_size = 0;
-    s->lily_items_start = 0;
-    current += s->lily_items_capacity * sizeof(struct LilyMatchItem);
+    s->lily_ctx.size = 0;
+    s->lily_ctx.start = 0;
+    current += s->lily_ctx.capacity * sizeof(struct LilyMatchItem);
+
+    current = ROUNDUP_PTR(current, 16);
+    s->lily_for_teddy_ctx.items = (struct LilyMatchItem *)current;
+    if (s->lily_for_teddy_ctx.capacity == 0) {
+        s->lily_for_teddy_ctx.items = NULL;
+    }
+    s->lily_for_teddy_ctx.size = 0;
+    s->lily_for_teddy_ctx.start = 0;
+    current += s->lily_for_teddy_ctx.capacity * sizeof(struct LilyMatchItem);
 
     *scratch = s;
 
@@ -380,9 +391,14 @@ hs_error_t HS_CDECL hs_alloc_scratch(const hs_database_t *db,
         proto->deduper.log_size = rose->dkeyLogSize;
     }
 
-    if (LILY_ITEMS_COUNT > proto->lily_items_capacity) { // 理应取自rose属性，依据规则推导出
+    if (LILY_ITEMS_COUNT > proto->lily_ctx.capacity) { // 理应取自rose属性，依据规则推导出
         resize = 1;
-        proto->lily_items_capacity = LILY_ITEMS_COUNT;
+        proto->lily_ctx.capacity = LILY_ITEMS_COUNT;
+    }
+
+    if (LILY_ITEMS_COUNT > proto->lily_for_teddy_ctx.capacity) { // 理应取自rose属性，依据规则推导出
+        resize = 1;
+        proto->lily_for_teddy_ctx.capacity = LILY_ITEMS_COUNT;
     }
 
     if (resize) {
