7c315762e0c9a22b7b21702139c8ea1b5351600f
guangsen <jiguangsen1998@126.com>
2026-05-15 16:46:56 +0800

    修改x86短规则旁路问题

--- src/hs.cpp
+++ src/hs.cpp
@@ -278,7 +278,7 @@ fat_hs_compile_multi_int(const char *const *expressions, const unsigned *flags,
         for (unsigned int i = 0; i < elements; i++) {
             if (flags && (flags[i] & HS_FLAG_COMBINATION)) {
                 try {
-                    addExpression(x86_ng, i, expressions[i], flags[i],
+                    x86_addExpression(x86_ng, i, expressions[i], flags[i],
                                   ext ? ext[i] : nullptr, ids ? ids[i] : 0);
                 } catch (CompileError &e) {
                     /* Caught a parse error:
@@ -293,7 +293,7 @@ fat_hs_compile_multi_int(const char *const *expressions, const unsigned *flags,
         for (unsigned int i = 0; i < elements; i++) {
             if (!flags || !(flags[i] & HS_FLAG_COMBINATION)) {
                 try {
-                    addExpression(x86_ng, i, expressions[i], flags ? flags[i] : 0,
+                    x86_addExpression(x86_ng, i, expressions[i], flags ? flags[i] : 0,
                                   ext ? ext[i] : nullptr, ids ? ids[i] : 0);
                 } catch (CompileError &e) {
                     /* Caught a parse error:
